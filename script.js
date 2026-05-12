cfdcb285a0a122c8cbf613dab36630fd6c0cd1f9
goofy9901 <borissavicc789@gmail.com>
2026-05-12 12:32:29 +0200

    Still so confused

--- script.js
+++ script.js
@@ -5,7 +5,9 @@ const gameboard = (function () {
   let gameArr = Array(9).fill("");
   return {
     gameArr,
-    playRound() {},
+    playRound(n, sign) {
+      gameArr[n] = sign;
+    },
   };
 })();
 
@@ -15,3 +17,40 @@ const createPlayer = function (name, sign) {
     sign,
   };
 };
+
+const game = (function () {
+  const winningCombs = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+  ];
+  return {
+    winning() {
+      for (let i = 0; i < winningCombs.length; i++) {
+        const [a, b, c] = winningCombs[i];
+        if (
+          gameboard.gameArr[a] == gameboard.gameArr[b] &&
+          gameboard.gameArr[a] == gameboard.gameArr[c] &&
+          gameboard.gameArr[b] == gameboard.gameArr[c] &&
+          gameboard.gameArr[a] !== "" &&
+          gameboard.gameArr[b] !== "" &&
+          gameboard.gameArr[c] !== ""
+        ) {
+          console.log(`${gameboard.gameArr[a]} won`);
+          return true;
+        }
+      }
+    },
+    isTie() {
+      const full = gameboard.gameArr.every((field) => field !== "");
+      if (full) {
+        console.log(`Game is tie!`);
+      }
+    },
+  };
+})();
